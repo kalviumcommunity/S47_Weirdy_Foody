1ed3517610739f2f526c2b1f1354fab2bbb7af19
aniketgoyal51 <141112246+aniketgoyal51@users.noreply.github.com>
2024-01-19 11:44:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express=require("express");
 const app=express();
-const port=3000
 
 app.get("/ping",(req,res)=>{
     res.send("Weirdy Foody");
 })
 
-app.listen(port, () => {
-    console.log(`server is running on PORT: ${port}`);
-});
+var server= app.listen(3000);
