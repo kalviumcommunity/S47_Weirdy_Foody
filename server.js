589ecccf31ad278a158a0a48d4375143763c8adb
aniketgoyal51 <aniketgoyal791@gmail.com>
2024-01-27 01:37:45 +0530

    Stablished the connection between dataBase and server

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express=require("express");
+const mongoose= require("mongoose");
 const app=express();
 const port=3000
 
-app.get("/ping",(req,res)=>{
-    res.send("Weirdy Foody");
+const data='mongodb+srv://aniketg:FOT8yjNAFZj39q0m@cluster0.wikddym.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.connect(data)
+.then(()=>{
+    try {
+        app.get("/",(req,res)=>{
+            res.json({message:"server is running"})
+        })
+    } catch (error) {
+        app.get("/",(req,res)=>{
+            res.json({message:error,message:"server is not running"})
+        })
+    }
 })
 
 app.listen(port, () => {
