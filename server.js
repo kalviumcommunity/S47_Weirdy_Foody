088817089bab6048f97fa89733e12ae509e1f7b0
aniketgoyal51 <aniketgoyal791@gmail.com>
2024-01-27 10:11:21 +0530

    Stablished the connection between dataBase and server

--- server.js
+++ server.js
@@ -5,17 +5,13 @@ const port=3000
 
 const data='mongodb+srv://aniketg:FOT8yjNAFZj39q0m@cluster0.wikddym.mongodb.net/?retryWrites=true&w=majority'
 
-mongoose.connect(data)
-.then(()=>{
-    try {
-        app.get("/",(req,res)=>{
-            res.json({message:"server is running"})
-        })
-    } catch (error) {
-        app.get("/",(req,res)=>{
-            res.json({message:error,message:"server is not running"})
-        })
-    }
+app.get("/",(req,res)=>{
+    mongoose.connect(data)
+    .then(()=>{
+        res.json({message:"server is running"})})
+    .catch((error)=>{
+        res.json({message:error,message:"server is not running"})
+    })
 })
 
 app.listen(port, () => {
